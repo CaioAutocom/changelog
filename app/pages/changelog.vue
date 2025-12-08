1877ce0cfa18e2928eae20945eb99a45876a33e4
Caio Autocom <caio.mendes@autocom-mg.com.br>
2025-12-08 13:05:58 -0300

    adiciona componente de changelog

--- app/pages/changelog.vue
+++ app/pages/changelog.vue
@@ -1,10 +1,38 @@
+<script setup lang="ts">
+const versions = ref([
+  {
+    title: 'Versão 3.17',
+    description: 'Adiciona módulo de produtos e melhorias na performance geral do sistema.',
+    date: '2025-04-27',
+    to: 'https://nuxt.com/blog/v3-17',
+    target: '_blank',
+    ui: {
+      container: 'max-w-lg'
+    }
+  },
+  {
+    title: 'Versão 3.16',
+    description: 'Adiciona melhorias na interface do usuário e correções de bugs.',
+    date: '2025-03-07',
+    to: 'https://nuxt.com/blog/v3-16',
+    target: '_blank',
+    ui: {
+      container: 'max-w-lg'
+    }
+  },
+  {
+    title: 'Versão 3.15',
+    description: 'Adiciona melhorias na interface do usuário e correções de bugs.',
+    date: '2024-12-24',
+    to: 'https://nuxt.com/blog/v3-15',
+    target: '_blank',
+    ui: {
+      container: 'max-w-lg'
+    }
+  }
+])
+</script>
+
 <template>
-  <div>
-    <UChangelogVersion title="Introducing Nuxt UI v3" />
-    <UChangelogVersion
-      title="Introducing Nuxt UI v3"
-      description="Nuxt UI v3 is out! After 1500+ commits, this major redesign brings improved accessibility, Tailwind CSS v4 support, and full Vue compatibility."
-    />
-    <h1>Hi Lorena</h1>
-  </div>
+  <UChangelogVersions :versions="versions" class="mx-auto max-w-4xl" />
 </template>
